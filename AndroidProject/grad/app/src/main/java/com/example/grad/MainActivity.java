82a3825ea349fbee3b44826cf8dd66c914705dee
Wichan <wichan7@naver.com>
2020-03-02 20:54:44 +0900

    1. intent 예시

--- AndroidProject/grad/app/src/main/java/com/example/grad/MainActivity.java
+++ AndroidProject/grad/app/src/main/java/com/example/grad/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.grad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_gotoSelect;
+    // activity_main.xml의 btn_gotoSelect와 연결할 객체 선언.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_gotoSelect = findViewById(R.id.btn_gotoSelect); // activity_main = R 같은의미인듯
+        btn_gotoSelect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
