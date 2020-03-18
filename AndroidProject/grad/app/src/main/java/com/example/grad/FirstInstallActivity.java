2bc3dffb4ddb5eb681ff870596df3f8a48c2ed09
Wichan <wichan7@naver.com>
2020-03-18 20:14:19 +0900

    2020_03_18 강위찬 1. 레이아웃 틀 미리 생성 2. 작업 설명ppt 추가

---
+++ AndroidProject/grad/app/src/main/java/com/example/grad/FirstInstallActivity.java
@@ -0,0 +1,14 @@
+package com.example.grad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstInstallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_install);
+    }
+}
