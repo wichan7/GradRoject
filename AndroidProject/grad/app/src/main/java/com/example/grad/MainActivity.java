744c8757dd1fafed58c57aff6f3ee23dc1e81fa2
wichan <wichan7@naver.com>
2020-04-30 13:44:10 +0900

    main에 바로가기 추가

--- AndroidProject/grad/app/src/main/java/com/example/grad/MainActivity.java
+++ AndroidProject/grad/app/src/main/java/com/example/grad/MainActivity.java
@@ -7,24 +7,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button btn_gotoSelect;
-    // activity_main.xml의 btn_gotoSelect와 연결할 객체 선언.
+    private Button btn_gotoPassengerCall;
+    private Button btn_gotoDriverCallList;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_gotoSelect = findViewById(R.id.btn_gotoSelect); // activity_main = R 같은의미인듯
-        btn_gotoSelect.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FirstInstallActivity.class);
+        btn_gotoSelect = findViewById(R.id.btn_gotoSelect);
+        btn_gotoSelect.setOnClickListener(this);
+        btn_gotoPassengerCall = findViewById(R.id.btn_gotoPassengerCall);
+        btn_gotoPassengerCall.setOnClickListener(this);
+        btn_gotoDriverCallList = findViewById(R.id.btn_gotoDriverCallList);
+        btn_gotoDriverCallList.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btn_gotoSelect:
+                intent = new Intent(MainActivity.this, FirstInstallActivity.class);
                 startActivity(intent);
                 finish();
-            }
-        });
+                break;
+
+            case R.id.btn_gotoPassengerCall:
+                intent = new Intent(MainActivity.this, PassengerCallActivity.class);
+                startActivity(intent);
+                finish();
+
+            case R.id.btn_gotoDriverCallList:
+                intent = new Intent(MainActivity.this, DriverCallListActivity.class);
+                startActivity(intent);
+                finish();
+        }
     }
 }
